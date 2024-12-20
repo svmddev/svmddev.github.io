2a33c6d93334ea20d5b8d3456f74faae335251b4
monsieurverde <domverdemgmt@gmail.com>
2024-12-20 17:31:49 -0500

    fresh start

---
+++ precache-manifest.fc67e614daaa5e751e90690d0cd53541.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f05b4255a4c596f019f917233f83611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c3097bb8d89e2be207e",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "b5b2c9438d3ba07ba59b",
+    "url": "/static/css/main.c6001462.chunk.css"
+  },
+  {
+    "revision": "6c3097bb8d89e2be207e",
+    "url": "/static/js/2.2703e47b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2703e47b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b2c9438d3ba07ba59b",
+    "url": "/static/js/main.b3e31b61.chunk.js"
+  },
+  {
+    "revision": "ea7e859ac2f54974f198",
+    "url": "/static/js/runtime-main.926d109b.js"
+  }
+]);
