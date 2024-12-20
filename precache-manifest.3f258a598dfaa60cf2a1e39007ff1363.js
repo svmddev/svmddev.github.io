69c04907e85a97618507331e2fc79e405db61766
monsieurverde <domverdemgmt@gmail.com>
2024-12-20 18:39:08 -0500

    hope

---
+++ precache-manifest.3f258a598dfaa60cf2a1e39007ff1363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa34e562ab784acc8bb2fa4e5b0c5e9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "982108171702585872f4",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "982108171702585872f4",
+    "url": "/static/js/2.86d23d6c.chunk.js"
+  },
+  {
+    "revision": "a27a10e52e3d9ffc0e4a7c94b342b1f1",
+    "url": "/static/js/2.86d23d6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "381d5ab3a5a7b7e3d55d",
+    "url": "/static/js/main.5e671304.chunk.js"
+  },
+  {
+    "revision": "ea7e859ac2f54974f198",
+    "url": "/static/js/runtime-main.926d109b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc67e614daaa5e751e90690d0cd53541.js"
+  "/precache-manifest.3f258a598dfaa60cf2a1e39007ff1363.js"
 );
 
 self.addEventListener('message', (event) => {
